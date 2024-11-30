07627fb1da21ab2b75c0eb9598301cc7b194f67f
Izzan Nawa Syarif <izzan.nsyarif@gmail.com>
2024-11-30 19:44:39 +0700

    Top level not done

--- SRC/TopLevel.vhd
+++ SRC/TopLevel.vhd
@@ -4,14 +4,33 @@ use IEEE.numeric_std.all;
 
 entity TopLevel is
     port (
-        
+        clk : in std_logic;
+        op  : in std_logic_vector(7 downto 0);
+        rst : in std_logic;
+        start  : in std_logic;
+        status : out std_logic
     );
 end entity TopLevel;
 
 architecture rtl of TopLevel is
-    
+    type state is (idle, read_module, compress, write_module done);
+    signal current_state, next_state: state := Idle;
 begin
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                current_state <= Idle;
+            elsif start = '1' then
+                current_state <= next_state;
+            end if;
+        end if;
+    end process
+
+    process(current_state, start)
+    begin
     
+    end process
     
     
 end architecture rtl;
